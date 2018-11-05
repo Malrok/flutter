a78179142626f5ca34715180f232a18b5249f22f
Simon Bernardin <simon.bernardin@moventes.com>
2018-11-05 21:07:30 +0100

    Added a detail screen

--- lib/userslist.dart
+++ lib/userslist.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterr/userdetail.dart';
 
 class UsersList extends StatefulWidget {
   @override
@@ -7,13 +8,10 @@ class UsersList extends StatefulWidget {
 }
 
 class UsersListState extends State<UsersList> {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Users\' list')
-        ),
+        appBar: new AppBar(title: const Text('Users\' list')),
         body: buildList());
   }
 
@@ -28,10 +26,19 @@ class UsersListState extends State<UsersList> {
             padding: const EdgeInsets.all(16.0),
             itemBuilder: (context, index) {
               return new ListTile(
-                leading: new Image.network(snapshot.data.documents[index]['picture'], width: 64.0, height: 64.0),
-                title: new Text(snapshot.data.documents[index]['first_name']),
-                subtitle: new Text(snapshot.data.documents[index]['last_name']),
-              );
+                  leading: new Image.network(
+                      snapshot.data.documents[index]['picture'],
+                      width: 64.0,
+                      height: 64.0),
+                  title: new Text(snapshot.data.documents[index]['first_name']),
+                  subtitle:
+                      new Text(snapshot.data.documents[index]['last_name']),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => UserDetail(id: snapshot.data.documents[index].documentID))
+                    );
+                  });
             },
             itemCount: snapshot.data.documents.length,
           );
